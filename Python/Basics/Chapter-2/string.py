05bea2da051ea9a61d330d8e3b5524a526f44430
Prajwal Dhage <psdhage1@gmail.com>
2026-07-19 16:19:22 +0530

    Add Chapter 2

---
+++ Python/Basics/Chapter-2/string.py
@@ -0,0 +1,17 @@
+# Date: 8-July-2026
+
+# String Operations
+
+name = "prajwal dhage"
+print(name.title())
+
+first_name = "prajwal"
+last_name = "dhage"
+
+print(first_name + " " + last_name)
+
+full_name = first_name + " " + last_name
+
+print (full_name.upper())
+print (full_name.lower())
+print (full_name.title())
